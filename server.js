a53a263597d6951bfcdd3487a5e18adb7dfc5d39
Blessing Tanyaradzwa Hove <86293408+Blssngx@users.noreply.github.com>
2023-11-25 00:06:19 +0200

    Add nodemon to package.json scripts

--- server.js
+++ server.js
@@ -19,45 +19,51 @@ app.get('/candlesticks', async (req, res) => {
 });
 
 async function getCandlestickData(pairs) {
-  const yesterday = new Date();
-  yesterday.setDate(yesterday.getDate() - 1);
-
-  const formattedDate = yesterday.toISOString().split('T')[0];
-
-  const candlestickDataPromises = pairs.split(',').map(async (pair) => {
-    const [baseCurrency, quoteCurrency] = pair.split('/');
-    const response = await axios.get(
-      `https://min-api.cryptocompare.com/data/v2/histoday`,
-      {
-        params: {
-          fsym: baseCurrency,
-          tsym: quoteCurrency,
-          limit: 2, // 2 days to ensure we have enough data for the previous day
-        },
-      }
-    );
-
-    const coinData = response.data.Data.Data;
-
-    // Retrieve candlestick data for the previous day
-    const candlestickData = coinData
-      .filter((data) => data.time * 1000 > yesterday.getTime())
-      .map((data) => ({
-        time: data.time * 1000,
-        open: data.open,
-        high: data.high,
-        low: data.low,
-        close: data.close,
-      }));
-
-    return {
-      pair,
-      candlestickData,
-    };
-  });
-
-  return Promise.all(candlestickDataPromises);
-}
+    const today = new Date();
+    const yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+    const dayBeforeYesterday = new Date();
+    dayBeforeYesterday.setDate(dayBeforeYesterday.getDate() - 2);
+  
+    const formattedToday = today.toISOString().split('T')[0];
+    const formattedYesterday = yesterday.toISOString().split('T')[0];
+    const formattedDayBeforeYesterday = dayBeforeYesterday.toISOString().split('T')[0];
+  
+    const candlestickDataPromises = pairs.split(',').map(async (pair) => {
+      const [baseCurrency, quoteCurrency] = pair.split('/');
+      const response = await axios.get(
+        `https://min-api.cryptocompare.com/data/v2/histoday`,
+        {
+          params: {
+            fsym: baseCurrency,
+            tsym: quoteCurrency,
+            limit: 3, // 3 days to ensure we have enough data for the last three days
+          },
+        }
+      );
+  
+      const coinData = response.data.Data.Data;
+  
+      // Retrieve candlestick data for the last three days
+      const candlestickData = coinData
+        .filter((data) => data.time * 1000 >= dayBeforeYesterday.getTime())
+        .map((data) => ({
+          time: data.time * 1000,
+          open: data.open,
+          high: data.high,
+          low: data.low,
+          close: data.close,
+        }));
+  
+      return {
+        pair,
+        candlestickData,
+      };
+    });
+  
+    return Promise.all(candlestickDataPromises);
+  }
+  
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
